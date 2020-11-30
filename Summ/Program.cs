059443bf0472d633e49114aa7fa394661279f0a3
Rev1ved <Rev1ved911@gmail.com>
2020-11-30 11:51:55 +0700

    Summ complete

--- Summ/Program.cs
+++ Summ/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+Эберлинг Виктор Викторович
+
+3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +13,15 @@ namespace Summ
 {
     class Program
     {
+        static void cursorPosition(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+        }
+        static void printResult(int result)
+        {
+            cursorPosition(50, 0);
+            print($"Текущий результат: {result}");
+        }
         static void pause()
         {
             Console.ReadKey();
@@ -19,16 +33,27 @@ namespace Summ
         static void Main(string[] args)
         {
             #region ConsoleColor
+            Console.SetWindowSize(80, 20);
+            Console.SetBufferSize(80, 20);
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Clear();
             #endregion
-            int num = 1;
+            int num = 1, summ = 0;
+            print("Воводите числа:\n");
             while(num != 0)
             {
-                print("NaN");
-                pause();
+                cursorPosition(0, 1);
+                print("                                                                                                                              ");
+                cursorPosition(0, 1);
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > 0 & (num % 2 == 0))
+                    summ += num;
+                printResult(summ);
             }
-
+            Console.Clear();
+            cursorPosition(12, 9);
+            print($"Сумму всех введенных нечетных положительных чисел: {summ}");
+            pause();
         }
     }
 }
