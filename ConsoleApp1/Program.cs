21ea73936742a090adf9b793d756d54a75569e09
Rev1ved <Rev1ved911@gmail.com>
2020-11-30 10:36:54 +0700

    update minNumber

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -23,18 +23,27 @@ namespace minNumber
         {
             Console.Write(msg);
         }
-        static void printNum(double num)
+        static void printNum(string msg, double num)
         {
-            Console.WriteLine($"\nНаименьшее число: {num}");
+            Console.WriteLine(msg + num);
+        }
+        static void methodMaxNumbers(double first, double second, double third)
+        {
+            if (first > second & first > third)
+                printNum("\nНаибольшее число: ", first);
+            else if (second > first & second > third)
+                printNum("\nНаибольшее число: ", second);
+            else
+                printNum("\nНаибольшее число: ", third);
         }
         static void methodMinNumbers(double first, double second, double third)
         {
             if (first < second & first < third)
-                printNum(first);
+                printNum("\nНаименьшее число: ", first);
             else if (second < first & second < third)
-                printNum(second);
+                printNum("\nНаименьшее число: ", second);
             else
-                printNum(third);
+                printNum("\nНаименьшее число: ", third);
         }
         static void Main(string[] args)
         {
@@ -49,6 +58,7 @@ namespace minNumber
             print("Введите третье число: ");
             double thirdNumber = Convert.ToDouble(Console.ReadLine());
             methodMinNumbers(firstNumber, secondNumber, thirdNumber);
+            methodMaxNumbers(firstNumber, secondNumber, thirdNumber);
             print("\nPress any key...");
             pause();
         }
--- digitsNumbers/Program.cs
+++ digitsNumbers/Program.cs
@@ -8,8 +8,23 @@ namespace digitsNumbers
 {
     class Program
     {
+        static void print(string msg)
+        {
+            Console.Write(msg);
+        }
+        static void pause()
+        {
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
+            #region ConsoleColor
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            #endregion
+            print("Test");
+            print("\nPress any key...");
+            pause();
         }
     }
 }
