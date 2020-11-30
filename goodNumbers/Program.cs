271cdb266bb279587aea0369176a0d73eaa0df75
Rev1ved <Rev1ved911@gmail.com>
2020-11-30 15:50:26 +0700

    X3

--- goodNumbers/Program.cs
+++ goodNumbers/Program.cs
@@ -4,6 +4,8 @@
 6. *Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000. 
 Хорошим называется число, которое делится на сумму своих цифр. 
 Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+
+Чесно, нифига не понял с этой суммой чисел. Х(
 */
 
 using System;
@@ -16,8 +18,34 @@ namespace goodNumbers
 {
     class Program
     {
+        static void print(string msg)
+        {
+            Console.Write(msg);
+        }
+        static void pause()
+        {
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
+            print($"{DateTime.Now}");
+            int num = 10000, sum = 0, i = 0;
+            while (num != 0)
+            {
+                int num1 = num;
+                while (num1 != 0)
+                {
+                    int digit = num1 % 10;
+                    sum += digit;
+                    num1 /= 10;
+                    print($"\n{sum}");
+                }
+                if((num % sum) == 0)
+                    i++;
+                num--;
+            }
+            print($"\n{i}");
+            pause();
         }
     }
 }
