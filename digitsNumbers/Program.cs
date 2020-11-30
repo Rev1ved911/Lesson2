b95074e4d7e5a889343b6f82a7504d5b9400490d
Rev1ved <Rev1ved911@gmail.com>
2020-11-30 11:15:18 +0700

    digitsNumbers complete

--- digitsNumbers/Program.cs
+++ digitsNumbers/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+Эберлинг Виктор Викторович
+
+2. Написать метод подсчета количества цифр числа.
+
+З.Ы. Немного не понял задания. 
+     Надеюсь сделал то, что просили. 
+     Если это число будет начинаться не с нуля, 
+     то можно начинать делить на 10, 
+     но есть число будет типа 0001010101, 
+     то деление уже не прокатит,
+     так что вывел просто длинну строки, 
+     не вижу никакой обходимости делать метод отдельный,
+     либо я не понял задания.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,8 +37,11 @@ namespace digitsNumbers
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Clear();
             #endregion
-            print("Test");
-            print("\nPress any key...");
+            print("Пишите: ");
+            string text = Console.ReadLine();
+            int length = text.Length;
+            print($"\n    В написанной вами строке {length} символов.");
+            print("\n\n    Press any key...");
             pause();
         }
     }
