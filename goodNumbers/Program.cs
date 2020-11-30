29d2d31fa50b906b38763c9b85d65834ef182080
Rev1ved <Rev1ved911@gmail.com>
2020-11-30 14:28:20 +0700

    goodNumbers start

---
+++ goodNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Эберлинг Виктор Викторович
+
+6. *Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+Хорошим называется число, которое делится на сумму своих цифр. 
+Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goodNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
