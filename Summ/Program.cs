9cefab04bfe569c880c816613ef040793ad13dec
Rev1ved <Rev1ved911@gmail.com>
2020-11-30 11:20:45 +0700

    Summ start

---
+++ Summ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summ
+{
+    class Program
+    {
+        static void pause()
+        {
+            Console.ReadKey();
+        }
+        static void print(string msg)
+        {
+            Console.Write(msg);
+        }
+        static void Main(string[] args)
+        {
+            #region ConsoleColor
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            #endregion
+            int num = 1;
+            while(num != 0)
+            {
+                print("NaN");
+                pause();
+            }
+
+        }
+    }
+}
